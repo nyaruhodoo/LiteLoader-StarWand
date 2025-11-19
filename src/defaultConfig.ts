1a8ca382ba8b3e57cdd09419d02cf39eb3931329
Nya <nyaruhodo9@gmail.com>
2025-11-19 22:28:13 +0800

    perf: 略微调整下默认配置

--- src/defaultConfig.ts
+++ src/defaultConfig.ts
@@ -17,7 +17,7 @@ export const defaultConfig: ConfigType = {
   minimumAmount: 200,
   randomDelay: {
     min: 2000,
-    max: 5000,
+    max: 4000,
   },
   redPackTextBlacklist: '挂&死&狗&测试',
   groupBlacklist: '',
